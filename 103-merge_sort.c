9ab57685e1b5e2c7393a1358e3dc3bd0b0e03510
Stanley Jacob <jaycobstano@gmail.com>
2022-10-07 03:09:03 -0700

    merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,100 +1,110 @@
 #include "sort.h"
-
+#include <stdio.h>
 /**
- * top_down_merge - merge sort algorithm
- * @array: the int array pointer header
- * @ini:  the array size
- * @midd:  the array size
- * @end:  the array size
- * @dest: the int array pointer header
+ *_calloc - this is a calloc function
+ *@nmemb: number of elemets
+ *@size: bit size of each element
+ *Return: pointer to memory assignement
  */
-void top_down_merge(int *array, size_t ini, size_t midd, size_t end, int *dest)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-size_t i = ini, j = midd, k;
-for (k = ini; k < end; k++)
-{
-if (i < midd && (j >= end || array[i] <= array[j]))
-{
-dest[k] = array[i];
-i = i + 1;
-}
-else
-{
-dest[k] = array[j];
-j = j + 1;
-}
-}
-}
+	unsigned int i = 0;
+	char *p;
 
-
+	if (nmemb == 0 || size == 0)
+		return ('\0');
+	p = malloc(nmemb * size);
+	if (p == '\0')
+		return ('\0');
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = '\0';
+	return (p);
+}
 /**
- * top_down_split - merge sort algorithm
- * @dest: the int array pointer header
- * @ini:  the array size
- * @end:  the array size
- * @array: the int array pointer header
+ *merge - make a merge
+ *@arr: one from start to mid
+ *@tmp: temp array used in merge, was created outside to
+ *optimize reducing the system calls
+ *@start: first element position
+ *@mid: array middle
+ *@end: last element position
+ **/
+void merge(int *arr, int *tmp, int start, int mid, int end)
+{
+	/*  sizes and temp arrays */
+	int size_left = mid - start + 1, size_right = end - mid;
+	int *array_left, *array_right;
+	/* counters */
+	int left, right, i = 0;
+
+	array_left = &tmp[0];
+	array_right = &tmp[size_right];
+	for (left = 0; left < size_left; left++)
+		array_left[left] = arr[start + left];
+	for (right = 0; right < size_right; right++)
+		array_right[right] = arr[mid + 1 + right];
+	left = 0, right = 0, i = start;
+	/* merging tmp arrays into main array*/
+	while (left < size_left && right < size_right)
+	{
+		if (array_left[left] <= array_right[right])
+			arr[i] = array_left[left], left++;
+		else
+			arr[i] = array_right[right], right++;
+		i++;
+	}
+	/* merging remaining left array into main array*/
+	while (left < size_left)
+		arr[i] = array_left[left], left++, i++;
+	/* merging remaining right array into main array*/
+	while (right < size_right)
+		arr[i] = array_right[right], right++, i++;
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(array_left, left);
+	printf("[right]: ");
+	print_array(array_right, right);
+	printf("[Done]: ");
+	print_array(&arr[start], left + right);
+}
+/**
+ *mergesort - function that sorts an array of integers
+ *in ascending order using the Merge sort algorithm
+ *@array: array of integers
+ *@tmp: temp array used in merge, was created outside to
+ *optimize reducing the system calls
+ *@start: fisrt element position
+ *@end: last element position
+ *Return: void
  */
-void top_down_split(int *dest, size_t ini, size_t end, int *array)
+void mergesort(int *array, int *tmp, int start, int end)
 {
-size_t middle, i;
-if (end - ini < 2)
-return;
-middle = (end + ini) / 2;
-top_down_split(array, ini, middle, dest);
-top_down_split(array, middle, end, dest);
-top_down_merge(dest, ini, middle, end, array);
-printf("Merging...\n");
-printf("[left]: ");
-for (i = ini; i < middle; i++)
-{
-printf("%d", dest[i]);
-if (i < middle - 1)
-printf(", ");
-}
-printf("\n[right]: ");
-for (i = middle; i < end; i++)
-{
-printf("%d", dest[i]);
-if (i < end - 1)
-printf(", ");
-}
-printf("\n[Done]: ");
-for (i = ini; i < end; i++)
-{
-printf("%d", array[i]);
-if (i < end - 1)
-printf(", ");
-else
-printf("\n");
-}
-}
+	int mid;
 
-
+	mid = (start + end) / 2;
+	if ((start + end) % 2 == 0)
+		mid--;
+	if (mid >= start)
+	{
+		mergesort(array, tmp, start, mid);
+		mergesort(array, tmp, mid + 1, end);
+		merge(array, tmp, start, mid, end);
+	}
+}
 /**
- * copy - merge sort algorithm
- * @array: the int array pointer header
- * @dest: the destination array
- * @size:  the array size
- */
-void copy(int *array, int *dest, size_t size)
-{
-size_t pos = 0;
-for (pos = 0; pos < size; pos++)
-{
-dest[pos] = array[pos];
-}
-}
-
-
-/**
- * merge_sort - merge sort algorith
- * @array: the int array pointer heder
- * @size:  the array siz
+ *merge_sort - function that sorts an array of integers
+ *in ascending order using the Merge sort algorithm
+ *@size: size of the list
+ *@array: array of integers
+ *Return: void
  */
 void merge_sort(int *array, size_t size)
 {
-int *dest = malloc(sizeof(int) * size);
-copy(array, dest, size);
-top_down_split(dest, 0, size, array);
-free(dest);
+	int *tmp;
+
+	if (!array || size < 2)
+		return;
+	tmp = _calloc(size, sizeof(int));
+	mergesort(array, tmp, 0, size - 1);
+	free(tmp);
 }
